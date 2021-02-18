c6e1c283c18467c8857b2602cf58008215889014
Ethan Burns <burns.ethan@gmail.com>
2021-02-17 20:16:27 -0500

    Add the module name to FuncInst.String().

--- checker/string.go
+++ checker/string.go
@@ -285,6 +285,10 @@ func (f *FuncDecl) buildString(s *strings.Builder) *strings.Builder {
 }
 
 func (f *FuncInst) buildString(s *strings.Builder) *strings.Builder {
+	if f.Def.File.Mod.Imported {
+		s.WriteString(f.Def.Mod)
+		s.WriteRune(' ')
+	}
 	s.WriteString(f.Def.Name)
 	s.WriteRune('(')
 	for i, p := range f.T.Parms {
