7a0f661b593f2fc026ed5b8312ccbe56a1e571b2
Ethan Burns <burns.ethan@gmail.com>
2021-07-10 11:29:06 -0400

    Move os to sys/fs and add comments to exported definitions.
    All of the functions in os were file system specific, so just put them in a module about filesystems. It is sys/fs. This starts the convention that modules which wrap calls to the operating system are sub-directories of sys/.
    
    Types not specific to the syste implementation and their supporting functions are in non-sys modules: io and fs.
    
    Finally, io#write_result is re-worked to allow returning the number of bytes written even in the case of an error.

--- modules/sys/fs/file_posix.c
+++ modules/sys/fs/file_posix.c
@@ -17,24 +17,24 @@ static char* cstring(struct pea_string* str) {
 	return cstr;
 }
 
-void os__posix_eacces(int32_t *ret) { *ret = EACCES; }
-void os__posix_eexist(int32_t *ret) { *ret = EEXIST; }
-void os__posix_enoent(int32_t *ret) { *ret = ENOENT; }
-void os__posix_enotdir(int32_t *ret) { *ret = ENOTDIR; }
+void sys__fs__posix_eacces(int32_t *ret) { *ret = EACCES; }
+void sys__fs__posix_eexist(int32_t *ret) { *ret = EEXIST; }
+void sys__fs__posix_enoent(int32_t *ret) { *ret = ENOENT; }
+void sys__fs__posix_enotdir(int32_t *ret) { *ret = ENOTDIR; }
 
-void os__posix_strerror_r(int32_t n, struct pea_string* buf) {
+void sys__fs__posix_strerror_r(int32_t n, struct pea_string* buf) {
 	strerror_r(n, buf->data, buf->length);
 }
 
-void os__posix_o_rdonly(int32_t *ret) { *ret = O_RDONLY; }
-void os__posix_o_wronly(int32_t *ret) { *ret = O_WRONLY; }
-void os__posix_o_rdwr(int32_t *ret) { *ret = O_RDWR; }
-void os__posix_o_append(int32_t *ret) { *ret = O_APPEND; }
-void os__posix_o_create(int32_t *ret) { *ret = O_CREAT; }
-void os__posix_o_trunc(int32_t *ret) { *ret = O_TRUNC; }
-void os__posix_o_excl(int32_t *ret) { *ret = O_EXCL; }
+void sys__fs__posix_o_rdonly(int32_t *ret) { *ret = O_RDONLY; }
+void sys__fs__posix_o_wronly(int32_t *ret) { *ret = O_WRONLY; }
+void sys__fs__posix_o_rdwr(int32_t *ret) { *ret = O_RDWR; }
+void sys__fs__posix_o_append(int32_t *ret) { *ret = O_APPEND; }
+void sys__fs__posix_o_create(int32_t *ret) { *ret = O_CREAT; }
+void sys__fs__posix_o_trunc(int32_t *ret) { *ret = O_TRUNC; }
+void sys__fs__posix_o_excl(int32_t *ret) { *ret = O_EXCL; }
 
-void os__posix_open(struct pea_string* str, int32_t flags, int32_t perm, int32_t *ret) {
+void sys__fs__posix_open(struct pea_string* str, int32_t flags, int32_t perm, int32_t *ret) {
 	char* cstr = cstring(str);
 	for ( ; ; ) {
 		int fd = open(cstr, flags, perm);
@@ -51,7 +51,7 @@ void os__posix_open(struct pea_string* str, int32_t flags, int32_t perm, int32_t
 	}
 }
 
-void os__posix_close(int32_t fd, int32_t *ret) {
+void sys__fs__posix_close(int32_t fd, int32_t *ret) {
 	for ( ; ; ) {
 		if (close(fd) == 0) {
 			*ret = 0;
@@ -64,7 +64,7 @@ void os__posix_close(int32_t fd, int32_t *ret) {
 	}
 }
 
-void os__posix_read(int32_t fd, struct pea_string* buf, int64_t *ret) {
+void sys__fs__posix_read(int32_t fd, struct pea_string* buf, int64_t *ret) {
 	for ( ; ; ) {
 		ssize_t n = read(fd, buf->data, buf->length);
 		if (n >= 0) {
@@ -78,7 +78,7 @@ void os__posix_read(int32_t fd, struct pea_string* buf, int64_t *ret) {
 	}
 }
 
-void os__posix_write(int32_t fd, struct pea_string* buf, int64_t *ret) {
+void sys__fs__posix_write(int32_t fd, struct pea_string* buf, int64_t *ret) {
 	for ( ; ; ) {
 		ssize_t n = write(fd, buf->data, buf->length);
 		if (n >= 0) {
@@ -92,7 +92,7 @@ void os__posix_write(int32_t fd, struct pea_string* buf, int64_t *ret) {
 	}
 }
 
-void os__posix_unlink(struct pea_string* str, int32_t *ret) {
+void sys__fs__posix_unlink(struct pea_string* str, int32_t *ret) {
 	char* cstr = cstring(str);
 	for ( ; ; ) {
 		int res = unlink(cstr);
@@ -109,7 +109,7 @@ void os__posix_unlink(struct pea_string* str, int32_t *ret) {
 	}
 }
 
-void os__posix_fdopendir(int32_t fd, DIR** dir_ret, int32_t *ret) {
+void sys__fs__posix_fdopendir(int32_t fd, DIR** dir_ret, int32_t *ret) {
 	for ( ; ; ) {
 		DIR* dir = fdopendir(fd);
 		if (dir != NULL) {
@@ -124,7 +124,7 @@ void os__posix_fdopendir(int32_t fd, DIR** dir_ret, int32_t *ret) {
 	}
 }
 
-void os__posix_closedir(void* dir, int32_t *ret) {
+void sys__fs__posix_closedir(void* dir, int32_t *ret) {
 	for ( ; ; ) {
 		if (closedir(dir) == 0) {
 			*ret = 0;
@@ -137,7 +137,7 @@ void os__posix_closedir(void* dir, int32_t *ret) {
 	}
 }
 
-void os__posix_readdir(DIR* dir, struct pea_string* str, int32_t *ret) {
+void sys__fs__posix_readdir(DIR* dir, struct pea_string* str, int32_t *ret) {
 	extern void* pea_malloc(int);
 
 	struct dirent* ent = NULL;
@@ -164,7 +164,7 @@ void os__posix_readdir(DIR* dir, struct pea_string* str, int32_t *ret) {
 	}
 }
 
-void os__posix_mkdir(struct pea_string* str, int32_t perm, int32_t *ret) {
+void sys__fs__posix_mkdir(struct pea_string* str, int32_t perm, int32_t *ret) {
 	char* cstr = cstring(str);
 	for ( ; ; ) {
 		if (mkdir(cstr, perm) == 0) {
@@ -180,7 +180,7 @@ void os__posix_mkdir(struct pea_string* str, int32_t perm, int32_t *ret) {
 	}
 }
 
-void os__posix_rmdir(struct pea_string* str, int32_t *ret) {
+void sys__fs__posix_rmdir(struct pea_string* str, int32_t *ret) {
 	char* cstr = cstring(str);
 	for ( ; ; ) {
 		if (rmdir(cstr) == 0) {
