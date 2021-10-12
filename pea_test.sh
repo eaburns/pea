6f2c403fb737787075980d00daf79202bdd786fb
Ethan Burns <burns.ethan@gmail.com>
2021-10-12 17:43:34 -0400

    Cache test results in pea_test.sh.
    Currently pea_test.sh is quite slow. Much of the issue is the regexp exhaustive tests, which take some time to run. This change tries to address that by caching test results so that tests don't need to be rerun if the corresponding test binary hasn't changed.
    
    It works by storing test results in a cache file named by the sha1sum of the test binary name. If the cache file exists, skip running the test and just dump the output instead. If the file does not exist, run the test, outputting to the file.

--- pea_test.sh
+++ pea_test.sh
@@ -7,20 +7,56 @@ mods=$(
 	done) | sort | uniq
 )
 
-go build -o peac/peac peac/main.go
+go build -o peac/peac ./peac
 
 fail=0
 for mod in $mods; do
-	echo Testing $mod
-	if ! ./peac/peac -libpea ./libpea -test -root $root $mod; then
+	/bin/echo -n "Testing $mod … "
+	o=$(mktemp pea_tmp.XXXXXXXXXX)
+	if ! ./peac/peac -libpea ./libpea -test -root $root $mod > $o; then
 		fail=1
+		rm -f $root/$mod/*.pea_cache
+		echo failed to build
+		cat $o
+		rm $o
 		continue
 	fi
-	o=$(mktemp pea.XXXXXXXXXX)
-	if ! $root/$mod/$(basename $mod).test > $o; then
-		fail=1
-		cat $o
+	rm $o
+
+	bin=$root/$mod/$(basename $mod).test
+	# Use the first 6 characters of the sha1 only.
+	# This should be unique enough,
+	# and makes for file names that don't overflow
+	# columnated directory output.
+	#
+	# TODO: revisit sha1 truncation decision
+	# when we stop outputting compiler intermediate
+	# files into the source directory.
+	sha1=$(sha1sum $bin | awk '{ print substr($1,1,6) }')
+	pass_cache=$bin-$sha1-pass.pea_cache
+	fail_cache=$bin-$sha1-fail.pea_cache
+	if test -e $pass_cache; then
+		echo "pass (cached)"
+		continue
 	fi
+	if test -e $fail_cache; then
+		fail=1
+		echo "FAIL (cached)"
+		cat $fail_cache
+		continue
+	fi
+
+	rm -f $root/$mod/*.pea_cache
+	if ! $bin > $o; then
+		fail=1
+		echo FAIL
+		cat $o
+		cat $o > $fail_cache
+		rm $o
+		continue
+	fi
+	touch $pass_cache
+	echo pass
 	rm $o
 done
 
